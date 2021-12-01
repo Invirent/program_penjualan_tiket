216976df133bdda3a5da64e46c143e258066d6a6
Invirent <74411024+Invirent@users.noreply.github.com>
2021-12-01 18:59:53 +0700

    [Add]order_pesawat_page + promo_page

--- run.py
+++ run.py
@@ -10,5 +10,13 @@ def home_redirect():
 def home_render():
    return render_template('home.html')
 
+@app.route('/order')
+def order_render():
+   return render_template('order_pesawat_page.html')
+
+@app.route('/promo')
+def promo_render():
+   return render_template('promo_page.html')
+
 if __name__ == '__main__':
    app.run("127.0.0.1", 5000, debug=True)
